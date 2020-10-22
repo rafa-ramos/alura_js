c69906150f7b6c54d1e7c0337493ba7a5f60f2d5
Rafael Ramos <161553@upf.br>
2020-10-22 01:28:44 -0300

    Aula 4 - Eventos, Formularios e Criando Element

--- js/principal.js
+++ js/principal.js
@@ -46,5 +46,49 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 */
 
+/*  Função Nomeada:
+    
+    titulo.addEventListener("click", mostraMensagem);
+    function mostraMensagem() {
+        console.log("Olá eu fui clicado!");
+    };
 
+    Função Anônima:
 
+    titulo.addEventListener("click", function() {
+    console.log("olá eu fui clicado!");
+});
+*/
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function() {
+    //previne o comportamento padrão, não recarregando a página o enviar o formulário
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var pacienteTr = document.createElement("tr");
+    
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+
+    nomeTd.textContent = form.nome.value;
+    pesoTd.textContent = form.peso.value;
+    alturaTd.textContent = form.altura.value;
+    gorduraTd.textContent = form.gordura.value;
+
+    //appendChild() -> coloque como filho.
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.getElementById("tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+    
+    console.log(tabela);
+    
+});
